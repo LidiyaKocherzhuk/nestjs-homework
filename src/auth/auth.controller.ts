8b4dcbd0be3d864469f2b34178aa44f77237fc80
LidiyaKocherzhuk <lidiyaonufreyiv31@gmail.com>
2022-06-10 15:15:01 +0300

    homework3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Tokens } from '@prisma/client';
 
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { AuthService } from './auth.service';
@@ -11,19 +19,47 @@ import { LoginUserDto } from './dto/login-user.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Registration user' })
+  @ApiResponse({
+    schema: {
+      example: {
+        id: 19,
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphaG9ka2E4QGdtYWlsLmNvbSIsIm5hbWUiOiJMaWRpeWEiLCJpZCI6MjAsImlhdCI6MTY1NDg2MDA1OX0.QzctQ_aVrEoQQVVxx-AdAPI3yokcmyUEKCR9M1wBRIc',
+        refreshToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphaG9ka2E4QGdtYWlsLmNvbSIsIm5hbWUiOiJMaWRpeWEiLCJpZCI6MjAsImlhdCI6MTY1NDg2MDA1OX0.NHxzrRWP9YKTwGjpQVk3Tc-PdHY-3nRtGrpe1fBoipE',
+        userId: 20,
+      },
+    },
+  })
+  @HttpCode(HttpStatus.CREATED)
   @Post('/registration')
-  registration(@Body() userDto: CreateUserDto) {
+  registration(@Body() userDto: CreateUserDto): Promise<Tokens> {
     return this.authService.registration(userDto);
   }
 
+  @ApiOperation({ summary: 'Login user' })
+  @ApiResponse({
+    schema: {
+      example: {
+        id: 19,
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphaG9ka2E4QGdtYWlsLmNvbSIsIm5hbWUiOiJMaWRpeWEiLCJpZCI6MjAsImlhdCI6MTY1NDg2MDA1OX0.QzctQ_aVrEoQQVVxx-AdAPI3yokcmyUEKCR9M1wBRIc',
+        refreshToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphaG9ka2E4QGdtYWlsLmNvbSIsIm5hbWUiOiJMaWRpeWEiLCJpZCI6MjAsImlhdCI6MTY1NDg2MDA1OX0.NHxzrRWP9YKTwGjpQVk3Tc-PdHY-3nRtGrpe1fBoipE',
+        userId: 20,
+      },
+    },
+  })
+  @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   @Post('/login')
-  login(@Body() loginUser: LoginUserDto) {
+  login(@Body() loginUser: LoginUserDto): Promise<Tokens> {
     return this.authService.login(loginUser);
   }
 
-  @Post('/refresh')
-  refresh() {
-    return 'refreshed';
-  }
+  // @Post('/refresh')
+  // refresh() {
+  //   return 'refreshed';
+  // }
 }
--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,4 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginUserDto {
+  @ApiProperty({ example: 'user@gmail.com', description: 'email' })
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   public email: string;
+
+  @ApiProperty({ example: 'password', description: 'user12345*' })
+  @IsString()
+  @IsNotEmpty()
   readonly password: string;
 }
